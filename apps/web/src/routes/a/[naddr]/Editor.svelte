25389fcbd6174e70c8a5ba3f5cba245348a7c42c
fiatjaf <fiatjaf@gmail.com>
2024-07-20 20:51:52 -0300

    replace quill markdown extensions with quill-asciidoc.

--- apps/web/src/routes/a/[naddr]/Editor.svelte
+++ apps/web/src/routes/a/[naddr]/Editor.svelte
@@ -1,43 +1,42 @@
 <script lang="ts">
-    import 'quilljs-markdown/dist/quilljs-markdown-common-style.css';
-	import { NDKEvent, type Hexpubkey, type NDKRelaySet, type NostrEvent, NDKUser } from '@nostr-dev-kit/ndk';
-	import { onMount } from "svelte";
+	import {
+		NDKEvent,
+		type Hexpubkey,
+		type NDKRelaySet,
+		type NostrEvent,
+		NDKUser
+	} from '@nostr-dev-kit/ndk';
+	import { onMount } from 'svelte';
 	import { ndk } from '@/ndk';
 	import Input from '@/components/ui/input/input.svelte';
 	import CategoryDropdown from './CategoryDropdown.svelte';
 	import ContentEditor from '@/components/ContentEditor.svelte';
 
-    export let baseEvent: NDKEvent;
-    export let relaySet: NDKRelaySet | undefined = undefined;
-    export let content: string;
-    export let newContent = false;
-    export let title: string;
-    export let category: string | undefined;
+	export let baseEvent: NDKEvent;
+	export let relaySet: NDKRelaySet | undefined = undefined;
+	export let content: string;
+	export let newContent = false;
+	export let title: string;
+	export let category: string | undefined;
 
-    $: if (content && newContent) {
-        newContent = false;
-        // const range = quill.getSelection();
-        // quill.setText(content);
-        // quill.setSelection(range);
-    }
-
-    onMount(()=>{
-		// quill = new Quill('#quill', { theme: 'snow' });
-        // // const quillMarkdown = new QuillMarkdown(quill, markdownOptions)
+	$: if (content && newContent) {
+		newContent = false;
+		// const range = quill.getSelection();
 		// quill.setText(content);
-        // quill.on('text-change', () => {
-        //     content = quill.getText();
-        //     newContent = true;
-        // });
+		// quill.setSelection(range);
+	}
 
-        let currentUser: NDKUser;
-        $ndk.signer!.user().then((user) => currentUser = user);
-    })
+	onMount(() => {
+		let currentUser: NDKUser;
+		$ndk.signer!.user().then((user) => (currentUser = user));
+	});
 
-    let timer = 0;
-    setInterval(() => { timer++; }, 1000);
+	let timer = 0;
+	setInterval(() => {
+		timer++;
+	}, 1000);
 </script>
 
 <Input bind:value={title} />
-<ContentEditor bind:content={content} bind:newContent />
+<ContentEditor bind:content bind:newContent />
 <CategoryDropdown bind:value={category} />
